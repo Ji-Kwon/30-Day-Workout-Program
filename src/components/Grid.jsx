d9f3b42d10c2eebec6891cea2d12e4bdf3c7a7c6
Ji-Kwon <jacob.wilkinson16@gmail.com>
2025-06-17 19:44:21 -0400

    updates

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,46 +1,74 @@
+import React, { useState } from 'react'
 import { workoutProgram as training_plan } from '../utils/index.js'
+import WorkoutCard from './WorkoutCard.jsx';
 
 export default function Grid(){
-    const isLocked = true;
-    
+    const isLocked = true
+    const selectedWorkout = 5
+    const [restActivity, setRestActivity] = useState('rest')
+    const cycle = [
+                    'Upper Body',  // day%7 === 0
+                    'Lower Body',  // day%7 === 1
+                    'Rest',        // day%7 === 2
+                    'Upper Body',  // day%7 === 3
+                    'Lower Body',  // day%7 === 4
+                    'Rest',        // day%7 === 5
+                    'Rest'         // day%7 === 6
+                    ]
     return(
-        <div className="workout-grid-plan">
-            {Object.keys(training_plan).map((workout, workoutIndex)=>{
-                const cycle = [
-                'Upper Body',  // day%7 === 0
-                'Lower Body',  // day%7 === 1
-                'Rest',        // day%7 === 2
-                'Upper Body',  // day%7 === 3
-                'Lower Body',  // day%7 === 4
-                'Rest',        // day%7 === 5
-                'Rest'         // day%7 === 6
-                ];
-                const type = cycle[ workoutIndex % cycle.length ];
-
-                return (
-                    <button key={workoutIndex}>
-                        <div className='plan-card-header'>
-                            <p>Day {((workoutIndex/8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex + 1}</p>
-                        </div>
-                        {isLocked ? (
-                            <i className='fa-solid fa-lock'></i>
-                        ) : (
-                            workoutIndex % 3 === 0 ?(
-                                <i className='fa-solid fa-dumbbell'></i>
-                            ) :(
-                                workoutIndex % 3 === 1 ? (
-                                    <i className='fa-solid fa-weight-hanging'></i>
-                                ) : (
-                                    <i className='fa-solid fa-person-walking'></i>
+        <div>
+            <div className="mb-6">
+                <label className="block mb-2 font-semibold">
+                On rest days I want to work on:
+                </label>
+                <select
+                className="p-2 border rounded"
+                value={restActivity}
+                onChange={e => setRestActivity(e.target.value)}
+                >
+                <option value="rest">Rest</option>
+                <option value="cardio">Cardio</option>
+                <option value="plyometrics">Plyometrics</option>
+                </select>
+            </div>
+            <div className="workout-grid-plan">
+                {Object.keys(training_plan).map((workout, workoutIndex)=>{
+                    
+                    const type = cycle[ workoutIndex % cycle.length ];
+                    const trainingPlan = training_plan[workoutIndex];
+                    const dayNum = ((workoutIndex/8) <= 1) ? '0' + (workoutIndex + 1) : workoutIndex + 1;
+                    const icon = type === 'Upper Body' ? (
+                                    <i className='fa-solid fa-dumbbell'></i>
+                                ) :(
+                                    type === 'Lower Body' ? (
+                                        <i className='fa-solid fa-weight-hanging'></i>
+                                    ) : (
+                                        <i className='fa-solid fa-person-walking'></i>
+                                    )
                                 )
-                            )
-                        )}
-                        <div className='plan-card-header'>
-                            <h4><b>{type}</b></h4>
-                        </div>
-                    </button>
-                )
-            })}
+                            
+
+                    if (workoutIndex === selectedWorkout) {
+                        return(
+                            <WorkoutCard key={workoutIndex} trainingPlan={trainingPlan} type={type} workoutIndex={workoutIndex} dayNum={dayNum} icon={icon} restActivity={restActivity} />
+                        )
+                    }
+
+                    return (
+                        <button className={''+(isLocked ? 'inactive styling' : 'active styling')} key={workoutIndex}>
+                            <div className='plan-card-header'>
+                                <p>Day {dayNum}</p>
+                            </div>
+                            {isLocked ? (
+                                <i className='fa-solid fa-lock'></i>
+                            ) : (icon)}
+                            <div className='plan-card-header'>
+                                <h4><b>{type}</b></h4>
+                            </div>
+                        </button>
+                    )
+                })}
+            </div>
         </div>
     );
 }
--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -1,5 +1,140 @@
-export default function WorkoutCard(){
-    return(
-        <></>
-    )
+import React from "react";
+
+export default function WorkoutCard(props) {
+  const {
+    trainingPlan,
+    workoutIndex,
+    type,
+    dayNum,
+    icon,
+    restActivity
+  } = props;
+
+  const isRestOnly = workoutIndex % 7 === 2;
+  const { workout, plyometrics, cardio} = trainingPlan || {};
+
+  return (
+    <div className="workout-container">
+      <div className="workout-card card">
+        <div className="plan-card-header">
+          <p>Day {dayNum}</p>
+          {icon}
+        </div>
+        <div className="plan-card-header">
+          <h2><b>{type}</b></h2>
+        </div>
+      </div>
+
+      <div className="workout-grid">
+        {type !== 'Rest' ? (
+          <div>
+            <div className="exercise-name">
+              <h4>Workout</h4>
+            </div>
+            <h6>Sets</h6>
+            <h6>Goal Reps</h6>
+            {workout.map((ex, i) => {
+              return (
+                <React.Fragment key={i}>
+                  <div className="exercise-name">
+                    <p>{i + 1}. {ex.name}</p>
+                    <button className="help-icon">
+                        <i className="fa-regular fa-circle-question"/>
+                    </button>
+                  </div>
+                  <p className="exercise-info">Sets: {ex.sets}</p>
+                  <p className="exercise-info">Goal reps: {ex.reps}</p>
+                  <input className="weight-input" placeholder="Weight" />
+                  <input className="reps-input" placeholder="Reps Achieved" />
+                </React.Fragment>
+              );
+            })}
+          </div>
+
+        ) : isRestOnly ? (
+          <div>
+            <div className="exercise-name">
+              <h4>Rest</h4>
+            </div>
+            <h6>Do something productive!</h6>
+          </div>
+
+        ) : (
+          <div>
+            {restActivity === 'plyometrics' && plyometrics && (
+              <div>
+                <div className="exercise-name">
+                  <h4>Plyometrics</h4>
+                </div>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                {plyometrics.map((ex, i) => {
+                  return (
+                    <React.Fragment key={i}>
+                      <div className="exercise-name">
+                        <p>{i + 1}. {ex.name}</p>
+                        <button className="help-icon">
+                            <i className="fa-regular fa-circle-question"/>
+                        </button>
+                      </div>
+                      <p className="exercise-info">Sets: {ex.sets}</p>
+                      <p className="exercise-info">Goal Reps: {ex.reps}</p>
+                      { i=== 1
+                        ? <input className="weight-input" placeholder="Weight" />
+                        : null
+                      }
+                      <input className="reps-input" placeholder="Reps Achieved" />
+                    </React.Fragment>
+                  );
+                })}
+              </div>
+            )}
+
+            {restActivity === 'cardio' && cardio && (
+              <div>
+                <div className="exercise-name">
+                  <h4>Cardio</h4>
+                </div>
+                <h6>Time</h6>
+                <h6>Incline</h6>
+                {cardio.map((ex, i) => {
+                  return (
+                    <React.Fragment key={i}>
+                      <div className="exercise-name">
+                        <p>{i + 1}. {ex.name}</p>
+                        <button className="help-icon">
+                            <i className="fa-regular fa-circle-question"/>
+                        </button>
+                      </div>
+                      <p className="exercise-info">Time: {ex.time}</p>
+                      <p className="exercise-info">
+                        Incline: {ex.incline != null ? ex.incline : 0}
+                      </p>
+                      {i === 0 
+                      ? null 
+                      : <input className="weight-input" placeholder="Time Achieved"/>
+                      }
+                    </React.Fragment>
+                  );
+                })}
+              </div>
+            )}
+
+            {restActivity === 'rest' && (
+              <div>
+                <div className="exercise-name">
+                  <h4>Rest</h4>
+                </div>
+                <h6>Do something productive!</h6>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <div>
+        <button>Save & Exit</button>
+        <button disabled={true}>Complete</button>
+      </div>
+    </div>
+  );
 }
